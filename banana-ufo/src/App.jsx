f2fee78f371a761aa2829047b37cce9d6f81763b
Bonnie <moller.bon@gmail.com>
2024-02-20 14:56:14 +0100

    more clean up

--- banana-ufo/src/App.jsx
+++ banana-ufo/src/App.jsx
@@ -1,20 +1,16 @@
 import './App.css';
-import './components/Heading.css';
 import Btn from './Components/Buttons/Buttons.jsx';
 import Nav from './Components/Nav/Nav.jsx';
 import ImgArticle from './Components/ImgArticle/ImgArticle.jsx';
 import UfoList from './Components/UfoList/UfoList.jsx';
+import Header from './Components/Header/Header.jsx';
 
 const App = () => {
     return <>
         <Nav />        
-        
-        <h1 className='header'>🍌 Hello BananaWorld! 🛸</h1>
-
+        <Header/>      
         <ImgArticle />
-
         <Btn />        
-
         <UfoList />
     </>
 }
---
+++ banana-ufo/src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <>
+            <h1 className='header'>🍌 Hello BananaWorld! 🛸</h1>
+        </>
+    )
+}
+
+export default Header;
