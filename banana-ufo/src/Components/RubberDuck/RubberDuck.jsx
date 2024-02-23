860ba383b29e2bbdc3559e54b31c7bc89ecde534
Bonnie <moller.bon@gmail.com>
2024-02-23 16:01:38 +0100

    duck favourite color as background

--- banana-ufo/src/Components/RubberDuck/RubberDuck.jsx
+++ banana-ufo/src/Components/RubberDuck/RubberDuck.jsx
@@ -2,7 +2,7 @@
 const RubberDuck = ({name, species, fact, favColor}) => {
     return (
         <>
-            <section>
+            <section className={favColor}>
                 <h2>{name}</h2>
                 <p>Species: {species}</p>
                 <p>Ducksplain: {fact}</p>
--- banana-ufo/src/Components/RubberDuck/RubberDuckBox.jsx
+++ banana-ufo/src/Components/RubberDuck/RubberDuckBox.jsx
@@ -1,11 +1,12 @@
 import rubberDucks from './RubberDuck.json'
 import RubberDuck from './RubberDuck'
+import './RubberDuck.css';
 
 const RubberDuckBox = () => {
     const data = rubberDucks.data;
     return (
         <>
-        <article>
+        <article className='duckBox'>
             {data.map((rubberDuck) => (
                 <RubberDuck key={rubberDuck.name} name={rubberDuck.name} species={rubberDuck.species} 
                 fact={rubberDuck.fun_fact} favColor={rubberDuck.favourite_color}/>
