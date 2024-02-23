2cee754b78813fc6c2566b95988a9cddefd1788b
Bonnie <moller.bon@gmail.com>
2024-02-23 15:05:06 +0100

    like button cute animals

--- banana-ufo/src/Components/CuteAnimals/CuteAnimals.jsx
+++ banana-ufo/src/Components/CuteAnimals/CuteAnimals.jsx
@@ -1,9 +1,21 @@
-const CuteAnimals = ({name, species, image_url}) => {
+import { useState } from "react";
+
+const CuteAnimals = ({name, species, favFood, bestie, favSpot, image_url}) => {
+    const [likes, setLikes] = useState(null);
+
+    const handleBtnClick = () => {       
+        setLikes(likes + 1);
+    }
     return (
         <>
             <section className="CuteAnimalCard">
                 <h3>Name: {name}</h3>
                 <p>Species: {species}</p>
+                <p>Fav Snack: {favFood}</p>
+                <p>Bestie: {bestie}</p>
+                {favSpot ? <p>Favourite Hangout: {favSpot}</p> : ''}
+                <button onClick={() => handleBtnClick()}>Like</button>
+                <p>{likes}</p>
                 <img src={image_url}/>
             </section>
         </>
--- banana-ufo/src/Components/CuteAnimals/CuteAnimalsBox.jsx
+++ banana-ufo/src/Components/CuteAnimals/CuteAnimalsBox.jsx
@@ -10,7 +10,7 @@ const CuteAnimalsBox = () => {
                 <h2>Look, some cute Animals to brighten your day</h2>
                 <section className="CardBox">
                 {data.map((animal) => (
-                     <CuteAnimals key={animal.name} name = {animal.name} species = {animal.species} image_url={animal.image_url}/>
+                     <CuteAnimals key={animal.name} name = {animal.name} species = {animal.species} favFood = {animal.favourite_food} bestie={animal.best_friend} favSpot={animal.favourite_spot} image_url={animal.image_url}/>
                 ))}
                 </section>
             </section>
